eca76d1091f873c44039737eed8e7b53d392f216
SabehShaikh <121764329+SabehShaikh@users.noreply.github.com>
2023-10-19 18:07:42 +0500

    completed Portfolio website.

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -13,6 +13,34 @@ const opentab = (tabname) => {
 }
 
 
+var sidemenu = document.getElementById('side-menu');
+
+const openMenu = () => {
+    sidemenu.style.right = '0';
+}
+
+const closeMenu = () => {
+    sidemenu.style.right = '-200px';
+}
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxgCY3uq_AAZPnMtZmonhz1k9Qx5LKKtaU6J4NgduvZC5t4n8bLCqsxWjkYHPq_cjlV/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById('msg')
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            Swal.fire('Message sent succesfully!')
+            // setTimeout(function () {
+            //     msg.innerHTML = ""
+            // }, 5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+})
+
 
 // let tablinks = document.getElementsByClassName('tab-links');
 // let tabcontents = document.getElementsByClassName('tab-contents');
